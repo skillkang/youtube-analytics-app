dfe94aceb7f1e69205fbaf40c68dbf4dae0df386
skillkang <skillkang@hotmail.com>
2025-06-21 16:28:27 -0500

    Update youtube_service.py

--- youtube_service.py
+++ youtube_service.py
@@ -6,7 +6,16 @@ class YouTubeService:
         api_key = st.secrets["YOUTUBE_API_KEY"]
         self.youtube = build("youtube", "v3", developerKey=api_key)
 
-   def search_videos(self, query, max_results=10, published_after=None, order="relevance", duration=None):
+   def search_videos(
+    self,
+    query,
+    max_results=10,
+    published_after=None,
+    order="relevance",
+    duration=None,
+    relevance_language=None,
+    region_code=None
+):
     request = self.youtube.search().list(
         q=query,
         part="snippet",
@@ -14,10 +23,13 @@ class YouTubeService:
         maxResults=max_results,
         publishedAfter=published_after if published_after else None,
         order=order,
-        videoDuration=duration if duration else None
+        videoDuration=duration if duration else None,
+        relevanceLanguage=relevance_language if relevance_language else None,
+        regionCode=region_code if region_code else None,
     )
     response = request.execute()
     return response["items"]
+
 
     def get_video_details(self, video_ids):
         request = self.youtube.videos().list(
